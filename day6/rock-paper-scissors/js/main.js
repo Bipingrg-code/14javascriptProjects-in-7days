64d425ffbb5b51d7edebc9ef1077ec232524a707
Bpeen Gurung <bipin67678grg@gmail.com>
2022-08-30 23:15:45 +0545

    rock paper sessiors

---
+++ day6/rock-paper-scissors/js/main.js
@@ -0,0 +1,7 @@
+/*const friut = ["mango","apple","banana","graphs"]
+
+const randomFriuts = (friut) => {
+   const randomNumber =  Math.floor(Math.random() * friut.length)
+   return friut[randomNumber]
+} 
+console.log(randomFriuts(friut)); */
